f972c0c6575a3a4beb6a7706458a008e3214bc67
darrenleeleelee1 <darrenleeleelee1@gmail.com>
2020-10-19 23:14:02 +0800

    HTTP POST

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from typing import Optional
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
-
+import numpy as np
+import shutil
 
 class Item(BaseModel):
     name: str
@@ -24,29 +25,11 @@ async def main():
     return FileResponse(r"misdo.jpg")
 
 
-@app.post("/tmp/")
-def create_tmp(tmp: Tmp):
-    print("You did it.")
-    return {"tmp": tmp.x, "what": {"test": "is"}}
-
-
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
-
-
-@app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item, q: Optional[str] = None):
-    result = {"item_id": item_id, **item.dict()}
-    if q:
-        result.update({"q": q})
-    return result
-
-
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+async def create_upload_file(image: UploadFile = File(...)):
+    with open("destination.jpg", "wb") as buffer:
+         shutil.copyfileobj(image.file, buffer)
+    return image.filename
+
+
+
